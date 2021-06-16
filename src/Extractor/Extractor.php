90cb331019bc1961e40439b566825336d5337284
Simon Podlipsky <simon@podlipsky.net>
2021-06-16 12:52:03 +0300

    Require to pass a decoded message contents into Extractor

--- src/Extractor/Extractor.php
+++ src/Extractor/Extractor.php
@@ -4,19 +4,14 @@ declare(strict_types=1);
 
 namespace SimPod\JsonRpc\Extractor;
 
-use Psr\Http\Message\MessageInterface;
-
-use function Safe\json_decode;
-
 abstract class Extractor
 {
     /** @var array{id: string, jsonrpc: string, error?: array{code: int, message: string, data?: mixed}, method: string, params?: array<string, mixed>} */
     protected array $messageContents;
 
-    public function __construct(MessageInterface $message)
+    /** @param array{id: string, jsonrpc: string, error?: array{code: int, message: string, data?: mixed}, method: string, params?: array<string, mixed>} $messageContents */
+    public function __construct(array $messageContents)
     {
-        /** @var array{id: string, jsonrpc: string, error?: array{code: int, message: string, data?: mixed}, method: string, params?: array<string, mixed>} $contents */
-        $contents = json_decode((string) $message->getBody(), true);
-        $this->messageContents = $contents;
+        $this->messageContents = $messageContents;
     }
 }
